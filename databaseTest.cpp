be668220fc962ebcf25de902a74f8df77d73d1cf
Dimitri Medeiros <79457494+itznokx@users.noreply.github.com>
2024-03-21 01:44:16 -0300

    Update databaseTest.cpp

--- databaseTest.cpp
+++ databaseTest.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main (void){
 	try{
-		pqxx::connection C("user=rootgres password=<dimitri191002> host=<defaultdatabase1.crm8g62qilja.sa-east-1.rds.amazonaws.com> dbname=postgres target_session_attrs=read-write");
+		pqxx::connection C("user=rootgres password=dimitri191002 host=defaultdatabase1.crm8g62qilja.sa-east-1.rds.amazonaws.com dbname=postgres target_session_attrs=read-write");
 		if (C.is_open()){
 			cout << "Database opened with sucess!" << endl;
 		}else {
